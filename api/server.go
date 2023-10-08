bbc1b0ab740944b9e2a048be5a91203f24c922d1
goopheramit <amitwpu@gmail.com>
2023-10-08 12:23:35 +0530

    adding basic api's using gin

--- api/server.go
+++ api/server.go
@@ -14,6 +14,8 @@ func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
 	server.router = router
 	return server
 }
--- db/query/account.sql
+++ db/query/account.sql
@@ -7,6 +7,10 @@ INSERT INTO accounts (
   $1, $2, $3
 ) RETURNING *;
 
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
 -- name: GetAccountForUpdate :one
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1
